c4f259c1b362948e86de6ca2801fa5a1d1a3c696
Peter Varo <info@petervaro.com>
2014-10-16 07:24:20 +0200

    chat-lang auto-formatter added

---
+++ chatlang.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @id chat-lang@petervaro
+// @name The C Stackoverflow Chatroom's chat-lang auto-formatter
+// @namespace petervaro
+// @include http://chat.stackoverflow.com/rooms/54304/c
+// @include http://chat.stackoverflow.com/rooms/1/sandbox
+// @author Peter Varo
+// @version 0.1.9
+// @updateURL https://github.com/petervaro/stackoverflow_c_chat/blob/gh-pages/chatlang.user.js
+// @grant none
+// ==/UserScript==
+(function ()
+{
+    /* Constant values */
+    var input = document.getElementById('input');
+    var regex = /^(helloc|free|goto|return)/gi;
+    var link  = 'http://bit.ly/c_chat';
+
+    /* Create and bind callback to event */
+    input.addEventListener(
+        'keyup',
+        (function ()
+        {
+            var match = regex.exec(input.value);
+            if (match != null)
+            {
+                var text = input.value = '[`' + match[1] + '`](' + link + ');';
+                input.selectionStart = input.selectionEnd = text.length - 1;
+            }
+        }),
+        true);
+})();
