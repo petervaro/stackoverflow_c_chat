ddd9184a8dcf96f077a862383ecb6b51025af58b
Peter Varo <info@petervaro.com>
2014-10-19 19:50:16 +0200

    better abbreviation replace and big-o abbrevation

--- chatlang.user.js
+++ chatlang.user.js
@@ -5,7 +5,7 @@
 // @include http://chat.stackoverflow.com/rooms/54304/c
 // @include http://chat.stackoverflow.com/rooms/1/sandbox
 // @author Peter Varo
-// @version 0.2.6
+// @version 0.2.9
 // @updateURL https://raw.githubusercontent.com/petervaro/stackoverflow_c_chat/gh-pages/chatlang.user.js
 // @grant none
 // ==/UserScript==
@@ -15,29 +15,34 @@
     var input = document.getElementById('input');
     var pattern_lang = /^(helloc|free|goto|return)/gi;
     var link  = '`](http://bit.ly/c_chat);';
-    var hints = {faq  : '[Frequently Asked Questions]' +
-                        '(http://c-faq.com)',
-                 decl : '[C gibberish to english]' +
-                        '(http://cdecl.org)',
-                 ptr  : '[Everything about pointers]' +
-                         '(http://boredzo.org/pointers)',
-                 ref  : '[C Reference]' +
-                        '(http://en.cppreference.com/w/c/header)',
-                 std  : '[C11 Standard]' +
-                        '(http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf)',
-                 test : '[Unit-testing macros]' +
-                        '(http://www.jera.com/techinfo/jtns/jtn002.html)',
-                 ask  : '[Short, Self Contained, Compilable, Example]' +
-                        '(http://sscce.org)',
-                 room : 'Guidelines of this room' +
-                        '',
-                 chat : '[chat-lang]' +
-                        '(http://bit.ly/c_chat)'};
+    var abbreviations =
+    {
+        faq  : '[Frequently Asked Questions]' +
+               '(http://c-faq.com)',
+        decl : '[C gibberish to english]' +
+               '(http://cdecl.org)',
+        ptr  : '[Everything about pointers]' +
+               '(http://boredzo.org/pointers)',
+        ref  : '[C Reference]' +
+               '(http://en.cppreference.com/w/c/header)',
+        std  : '[C11 Standard]' +
+               '(http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf)',
+        test : '[Unit-testing macros]' +
+               '(http://www.jera.com/techinfo/jtns/jtn002.html)',
+        bigo : '[Space and Time Big-O Complexities]' +
+               '(http://www.bigocheatsheet.com)',
+        ask  : '[Short, Self Contained, Compilable, Example]' +
+               '(http://sscce.org)',
+        room : 'Guidelines of this room' +
+               '',
+        chat : '[chat-lang]' +
+               '(http://bit.ly/c_chat)'
+    };
 
     /* Construct pattern based on abbreviations */
     var pattern_abbr = '(^|[^`"\'\\w\\\\])(/(';
     var not_first = 0;
-    for (var abbreviation in hints)
+    for (var abbreviation in abbreviations)
     {
         /* If not the first iteration */
         if (not_first)
@@ -46,6 +51,14 @@
         not_first++;
     }
     pattern_abbr = new RegExp(pattern_abbr + '))', 'gi');
+
+    /* Replacer function */
+    function autoCompleteLink(_1, _2, _3, abbreviation)
+    {
+        /* HACK: the extra space is need, because of the lack of the negative
+                 look-behind in the regular expression => try to solve this! */
+        return ' ' + abbreviations[abbreviation];
+    }
 
     /* Create and bind callback to event */
     input.addEventListener(
@@ -62,9 +75,7 @@
             }
 
             /* If there is an abbreviation match */
-            match = pattern_abbr.exec(input.value);
-            if (match !== null)
-                input.value = input.value.replace(match[2], hints[match[3]]);
+            input.value = input.value.replace(pattern_abbr, autoCompleteLink);
         }),
         true);
 })();
