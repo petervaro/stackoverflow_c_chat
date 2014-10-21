0cb5fd54a41a059f2a70aef074c708d0f5032589
Peter Varo <info@petervaro.com>
2014-10-21 15:11:23 +0200

    better cheat-sheet, consistent titles, book link added, main site fixed

--- chatlang.user.js
+++ chatlang.user.js
@@ -5,53 +5,68 @@
 // @include http://chat.stackoverflow.com/rooms/54304/c
 // @include http://chat.stackoverflow.com/rooms/1/sandbox
 // @author Peter Varo
-// @version 0.3.1
+// @version 0.3.3
 // @updateURL https://raw.githubusercontent.com/petervaro/stackoverflow_c_chat/gh-pages/chatlang.user.js
 // @grant none
 // ==/UserScript==
 (function ()
 {
-    /* Constant values */
+    /* DOM objects */
     var input = document.getElementById('input');
+    var info  = document.getElementById('info');
+
+    /* Constant values */
     var pattern_lang = /^(helloc|free|goto|return)/gi;
     var link  = '`](http://bit.ly/c_chat);';
     var abbreviations =
     {
         faq  : ['Frequently Asked Questions',
                 'http://c-faq.com'],
-        decl : ['C gibberish to english',
+        decl : ['C Gibberish to English',
                 'http://cdecl.org'],
-        ptr  : ['Everything about pointers',
+        ptr  : ['Everything About Pointers',
                 'http://boredzo.org/pointers'],
         ref  : ['C Reference',
                 'http://en.cppreference.com/w/c/header'],
         std  : ['C11 Standard',
                 'http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf'],
-        test : ['Unit-testing macros',
+        test : ['Unit-testing Macros',
                 'http://www.jera.com/techinfo/jtns/jtn002.html'],
         bigo : ['Space and Time Big-O Complexities',
                 'http://www.bigocheatsheet.com'],
+        book : ['The Definitive C Book Guide and List',
+                'http://stackoverflow.com/questions/562303'],
         ask  : ['Short, Self Contained, Compilable, Example',
                 'http://sscce.org'],
-        room : ['Guidelines of this room',
-                ''],
-        chat : ['chat-lang',
+        room : ['Draft of Guidelines of This Room',
+                'https://github.com/petervaro/stackoverflow_c_chat/blob/gh-pages/rules.md'],
+        chat : ['Chat-Lang',
                 'http://bit.ly/c_chat']
     };
 
+    /* Build hierarchy of the cheat-sheet */
+    var notes = document.createElement('div');
+    info.insertBefore(notes, document.getElementById('roomdesc'));
 
-    /* Construct pattern based on abbreviations */
+    var notes_pre = notes.appendChild(document.createElement('pre'));
+    notes_pre.appendChild(document.createElement('hr'));
+    notes_pre.style.fontSize = '9.5px';
+
+    var notes_code = notes_pre.appendChild(document.createElement('code'));
+    notes_pre.appendChild(document.createElement('hr'));
+
+    /* Construct regex pattern based on abbreviations and
+       also build the cheat-sheet as well */
     var pattern_abbr = '(^|[^`"\'\\w\\\\])(/(';
     var not_first = 0;
     var values = [];
-    var notes = '';
     for (var abbreviation in abbreviations)
     {
         /* If not the first iteration */
         if (not_first++)
         {
             pattern_abbr += '|';
-            notes += '<br/>';
+            notes_code.appendChild(document.createElement('br'));
         }
         /* Add abbreviation to the pattern */
         pattern_abbr += abbreviation;
@@ -59,22 +74,10 @@
         values = abbreviations[abbreviation];
         abbreviations[abbreviation] = '[' + values[0] + '](' + values[1] + ')';
         /* Extend notes */
-        notes += '/' + abbreviation + (abbreviation.length > 3 ? '' : ' ') + ' =&gt; ' + values[0];
+        notes_code.appendChild(document.createElement('span')).innerHTML =
+            '/' + abbreviation + (abbreviation.length > 3 ? '' : ' ') + ' =&gt; ' + values[0];
     }
     pattern_abbr = new RegExp(pattern_abbr + '))', 'gi');
-
-
-    /* Place the cheat-sheet on the page */
-    var notes_pre  = document.createElement('pre');
-    notes_pre.style.fontSize = '9.5px';
-
-    var notes_code = notes_pre.appendChild(document.createElement('code'));
-    notes_code.innerHTML = notes;
-
-    document.getElementById('info').insertBefore(notes_pre, document.getElementById('roomdesc'));
-    notes_pre.parentNode.insertBefore(document.createElement('hr'), notes_pre);
-    notes_pre.parentNode.insertBefore(document.createElement('hr'), notes_pre.nextSibling);
-
 
     /* Replacer function */
     function autoCompleteLink(match, _1, _2, abbreviation)
