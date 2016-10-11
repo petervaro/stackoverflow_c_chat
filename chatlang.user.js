2a7a09e94346bc8e4378a899da25303f270caff6
petervaro <petervaro@sketchandprototype.com>
2016-10-11 17:05:41 +0200

    proper licensing (GPLv3) added

--- chatlang.user.js
+++ chatlang.user.js
@@ -3,7 +3,7 @@
 // @name The C Stackoverflow Chatroom's chat-lang auto-formatter
 // @namespace petervaro
 // @author Peter Varo
-// @version 0.4.7
+// @version 0.4.8
 // @downloadURL https://raw.githubusercontent.com/petervaro/stackoverflow_c_chat/gh-pages/chatlang.user.js
 // @updateURL https://raw.githubusercontent.com/petervaro/stackoverflow_c_chat/gh-pages/chatlang.user.js
 // @include http://chat.stackoverflow.com/rooms/54304/c
@@ -11,6 +11,23 @@
 // @run-at document-end
 // @grant none
 // ==/UserScript==
+
+/*
+Copyright (C) 2015-2016 Peter Varo and the StackOverflow C Chatroom Team
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program, most likely a file in the root directory, called 'LICENSE'.
+If not, see <http://www.gnu.org/licenses>.
+*/
 (function ()
 {
     "use strict";
